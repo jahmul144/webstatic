31a8a5e3a93236a47b36f1641b69f287552f8e73
jahmul144 <erikhummel14@gmail.com>
2017-06-05 16:31:09 +0000

    Adding Admin Docs

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 from django.views.generic import TemplateView
@@ -26,4 +26,5 @@ urlpatterns = [
 	url(r'^home/$', views.start),
 	url(r'^$',TemplateView.as_view(template_name='homepage.html')),
 	url(r'^about/', about_views.contact),
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
 ]
