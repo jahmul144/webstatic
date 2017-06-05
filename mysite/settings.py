e94b5cdadbab76439832a19832fbcc929c806a40
jahmul144 <erikhummel14@gmail.com>
2017-06-05 16:49:07 +0000

    Added docs to apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,13 +34,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mysite.about',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'mysite.about',
+	'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [
